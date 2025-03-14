257b53377366ccdeddb082d3d7f6b5284b4f815d
Tanveer Azam Ansari <tanveerazamansari@MacBookAir.lan>
2025-03-14 15:34:49 +0530

    Hashmap implementation

--- src/HashMap/Demo.java
+++ src/HashMap/Demo.java
@@ -1,4 +1,31 @@
 package HashMap;
 
 public class Demo {
+    public static void main(String[] args) {
+        HashMap<Integer, String> mp = new HashMap<Integer, String>(16);
+        mp.put(12, "Raj");
+        mp.put(10, "Rahul");
+        try {
+            String val = mp.get(12);
+            System.out.println("Value at 12 is " + val);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            String val = mp.get(10);
+            System.out.println("Value at 12 is " + val);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            String val = mp.get(9);
+            System.out.println("Value at 12 is " + val);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
 }
--- src/HashMap/HashMap.java
+++ src/HashMap/HashMap.java
@@ -1,4 +1,67 @@
 package HashMap;
 
-public class HashMap {
+import java.security.Key;
+import java.util.LinkedList;
+
+class Element<K, V> {
+    K key;
+    V value;
+    public Element(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
+
+
+public class HashMap<K, V> {
+    int size = 16;
+    LinkedList<Element<K, V>>[] list;
+    public HashMap() {
+
+    }
+
+    public HashMap(int size) {
+        this.size = size;
+        list = new LinkedList[size];
+        for (int i = 0; i < size; i++) {
+            list[i] = new LinkedList<Element<K, V>>();
+        }
+    }
+
+    public void put(K key, V value) {
+        int index = key.hashCode() % size;
+        LinkedList<Element<K, V>> listAtIndex = list[index];
+        for (Element<K, V> e: listAtIndex) {
+            if (e.getKey() == key) {
+                e.setValue(value);
+                return;
+            }
+        }
+        Element<K, V> element = new Element<>(key, value);
+        listAtIndex.add(element);
+    }
+
+    public V get(K key) throws IllegalArgumentException {
+        int index = key.hashCode() % size;
+        LinkedList<Element<K, V>> listAtIndex = list[index];
+        for (Element<K, V> e: listAtIndex) {
+            if (e.getKey() == key) {
+               return e.getValue();
+            }
+        }
+
+        throw new IllegalArgumentException("No value with this key");
+    }
 }
